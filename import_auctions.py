3a913a9b4cf8788f3def94d810fba746889d3a1c
Marwolf <cleako@gmail.com>
2018-07-28 18:26:25 -0400

    Added many new features, both steps for JSON from Blizzard API
    Still need to solve one part but it asks the user for manual copy and paste input for the time being.

--- import_auctions.py
+++ import_auctions.py
@@ -1,35 +1,69 @@
 #!/usr/bin/env python3
 
+import os
+from os import system, name
+
+# Install missing imports
+os.system('sudo -H pip3 install -r requirements.txt')
+
 import requests
 import json
-import pymysql.cursors
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+# Install Ubuntu Linux python3-pymysql if still cannot import pymysql (bug bypass)
+try:
+    import pymysql.cursors
+except ImportError:
+    os.system('sudo -H apt-get install python3-pymysql -y')
+    import pymysql.cursors
+
+# Defines the clear function
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+# Query the API for the recently generated Blizzard JSON file URL path
+url = requests.get("https://us.api.battle.net/wow/auction/data/stormreaver?locale=en_US&apikey=nv6n8njc8jus3gtrz2n5fw9fpgyfjgcj").content
+unicode_str = url.decode("utf8")
+encoded_str = unicode_str.encode("ascii",'ignore')
+soup = BeautifulSoup(encoded_str, "html.parser")
+file = re.findall('(?<=\:\").+?(?=\"\,)', format(soup))
+clear()
+print ("")
+print (file)
+print ("")
+# I can't get this to automatically set. Possibly related to format(soup) above adding brackets.
+file = input("Right click copy the URL above, paste it below, then press enter. \r\n\r\n")
 
 
-# Downloads the latest auctions
-file = 'http://auction-api-us.worldofwarcraft.com/auction-data/SECRET_API_KEY/auctions.json'
+# Downloads the latest auction JSON file
+#file = 'http://auction-api-us.worldofwarcraft.com/auction-data/9694c0d6b384d8aa647c840e4c8a3435/auctions.json'
 r = requests.get(file)
 with open('auctions.json', 'wb') as f:
   f.write(r.content)
 data = json.load(open('auctions.json'))
 
 
-# Connect to the database
+# Connect to the MySQL database
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='',
+                             password='root',
                              db='wolf',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
 
-# Empty the old auctions table database entries
+# Truncate the MySQL 'auctions' table database for a fresh import
 try:
    with connection.cursor() as cursor:
         query = "TRUNCATE `auctions`"
         cursor.execute(query)
 
 
-# Import the latest auction house data from the JSON file
+# Import the latest auction house data from the JSON file into the MySQL 'auctions' table
    with connection.cursor() as cursor:
        index = 0
        while index != None:
@@ -50,13 +84,14 @@ except IndexError:
     pass
 
 
-# Count number of auctions that were added
+# Count number of auctions that were added for a sanity check
 with connection.cursor() as cursor:
     query = "SELECT COUNT(`auc`) from `auctions`"
     cursor.execute(query)
     res = cursor.fetchone()
-    print(res)
+    clear()
+    print("Total auction house listings imported: ", res)
+    print ("")
 
-
-# Done
+# Finito!
 connection.close()
